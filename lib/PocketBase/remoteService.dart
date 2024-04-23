ca64aa2baa32f79ea3f2757e5939e838d1b4c219
ahmadesmaili <38140607+immrit@users.noreply.github.com>
2024-04-24 02:10:08 +0330

    register ok

--- lib/PocketBase/remoteService.dart
+++ lib/PocketBase/remoteService.dart
@@ -1,14 +1,14 @@
 import 'package:pocketbase/pocketbase.dart';
 
-final pb = PocketBase('http://127.0.0.1:8089');
+final pb = PocketBase('http://10.0.2.2:8090');
 
-Future<void> SignUpUserRemote() async {
+Future<void> SignUpUserRemote(String username, String email, String password,
+    String passwordConfirm) async {
   final body = <String, dynamic>{
-    'username': 'ahmad',
-    "email": "bob@example.com",
-    "password": "12345678",
-    "passwordConfirm": "12345678",
-    "name": "Bob Smith"
+    'username': username,
+    "email": email,
+    "password": password,
+    "passwordConfirm": passwordConfirm,
   };
   final record = await pb.collection('users').create(body: body);
   print(record);
--- lib/View/Screens/signupScreen.dart
+++ lib/View/Screens/signupScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:vista_notes/View/widgets/widgets.dart';
 
+import '../../PocketBase/remoteService.dart';
+
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
@@ -10,7 +12,9 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
+  TextEditingController passwordConfirm = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,18 +23,21 @@ class _SignUpScreenState extends State<SignUpScreen> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         VistaTextLogo(),
-        SizedBox(height: 50),
+        SizedBox(height: 40),
         Container(
             padding: EdgeInsets.only(top: 50),
             child: TextInputWidget(username, "نام کاربری", false)),
+        TextInputWidget(email, "ایمیل", false),
         TextInputWidget(password, "رمز عبور", true),
+        TextInputWidget(passwordConfirm, "تکرار رمز عبور", true),
         Container(
           margin: EdgeInsets.symmetric(horizontal: 20),
           width: double.infinity,
           height: 50,
           child: ElevatedButton(
-            onPressed: () {},
-            child: Text("ورود",
+            onPressed: () => SignUpUserRemote(
+                username.text, email.text, password.text, passwordConfirm.text),
+            child: Text("عضویت",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
@@ -39,6 +46,25 @@ class _SignUpScreenState extends State<SignUpScreen> {
               backgroundColor: Colors.black,
             ),
           ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // TextButton(onPressed: () {}, child: Text("ثبت نام")),
+            GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => const SignUpScreen()));
+                },
+                child: const Text(
+                  "ثبت نام ",
+                  style: TextStyle(color: Colors.blue, fontSize: 16),
+                )),
+            const Text(
+              "حساب کاربری ندارید؟",
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
         )
       ],
     )));
