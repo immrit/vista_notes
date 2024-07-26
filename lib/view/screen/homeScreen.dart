b17cda6acab21ccef289ec8999d74549b4e3fcfc
ahmadesmaili <38140607+immrit@users.noreply.github.com>
2024-07-26 17:46:03 +0330

    get Data

--- lib/view/screen/homeScreen.dart
+++ lib/view/screen/homeScreen.dart
@@ -15,39 +15,73 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  @override
-  Widget build(BuildContext context) {
-    var _loading = true;
-    final TextEditingController titleController = TextEditingController();
-    Future<void> _insertPost() async {
+  List<dynamic> notes = []; // لیست برای ذخیره نت ها
+
+  var _loading = true;
+  final TextEditingController titleController = TextEditingController();
+
+  Future<void> _getNotes() async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      final userId = supabase.auth.currentSession!.user.id;
+      final data = await supabase.from('Notes').select().eq('user_id', userId);
       setState(() {
-        _loading = true;
+        notes = data; // نت ها را در لیست ذخیره می کنیم
       });
-      final title = titleController.text.trim();
-      final user = supabase.auth.currentUser;
-      final updates = {
-        'id': user!.id,
-        'title': title,
-        'created_at': DateTime.now().toIso8601String(),
-      };
-      try {
-        await supabase.from('Notes').insert(updates);
-        if (mounted) context.showSnackBar('Successfully updated profile!');
-      } on PostgrestException catch (error) {
-        if (mounted) context.showSnackBar(error.message, isError: true);
-      } catch (error) {
-        if (mounted) {
-          context.showSnackBar('Unexpected error occurred', isError: true);
-        }
-      } finally {
-        if (mounted) {
-          setState(() {
-            _loading = false;
-          });
-        }
+    } on PostgrestException catch (error) {
+      if (mounted) context.showSnackBar(error.message, isError: true);
+    } catch (error) {
+      if (mounted) {
+        context.showSnackBar('Unexpected error occurred', isError: true);
+      }
+    } finally {
+      if (mounted) {
+        setState(() {
+          _loading = false;
+        });
       }
     }
+  }
 
+  Future<void> _insertPost() async {
+    setState(() {
+      _loading = true;
+    });
+    final title = titleController.text.trim();
+    final user = supabase.auth.currentUser;
+    final updates = {
+      'user_id': user!.id,
+      'title': title,
+      'created_at': DateTime.now().toIso8601String(),
+    };
+    try {
+      await supabase.from('Notes').insert(updates);
+      if (mounted) context.showSnackBar('Successfully updated profile!');
+    } on PostgrestException catch (error) {
+      if (mounted) context.showSnackBar(error.message, isError: true);
+    } catch (error) {
+      if (mounted) {
+        context.showSnackBar('Unexpected error occurred', isError: true);
+      }
+    } finally {
+      if (mounted) {
+        setState(() {
+          _loading = false;
+        });
+      }
+    }
+  }
+
+  initState() {
+    super.initState();
+    _getNotes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vista Notes'),
@@ -83,9 +117,16 @@ class _HomescreenState extends State<Homescreen> {
           ],
         ),
       ),
-      body: const Column(
-        children: [],
-      ),
+      body: _loading
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              itemCount: notes.length, // تعداد نت ها
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(notes[index]['title']), // نمایش عنوان نت
+                );
+              },
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog<String>(
@@ -103,8 +144,11 @@ class _HomescreenState extends State<Homescreen> {
                     const SizedBox(height: 15),
                     TextButton(
                       onPressed: () {
-                        _insertPost();
-                        Navigator.pop(context);
+                        setState(() {
+                          _insertPost().then((v) => _getNotes());
+
+                          Navigator.pop(context);
+                        });
                       },
                       child: const Text('post'),
                     ),
